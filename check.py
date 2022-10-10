428533960e3b573cbbc7201df71a257876e0dfdd
harmless-tech <28830529+harmless-tech@users.noreply.github.com>
2022-10-10 10:54:11 -0400

    Attempt not found fix

--- check.py
+++ check.py
@@ -4,6 +4,7 @@ import sys, os, urllib.request, json
 
 crates_io_url = "https://crates.io"
 crates_io_api = crates_io_url + "/api/v1/crates/{CRATE}/versions"
+github_api = "https://api.github.com/repos/{REPO}/commits/main"
 
 
 # Gets the latest version from index file.
@@ -35,6 +36,12 @@ def main(mode, index, crate):
         print("first arg must be stable or nightly")
         sys.exit(1)
 
+    repo = ""
+    if mode == "nightly":
+        sections = crate.split(":")
+        repo = sections[0]
+        crate = sections[1]
+
     # Get version from cache
     if not os.path.exists(crate + ".cache"):
         with open(crate + ".cache", "w"):
@@ -43,19 +50,19 @@ def main(mode, index, crate):
     with open(crate + ".cache", "r") as file:
         cached = file.read()
 
-    if cached == "":
-        res = urllib.request.urlopen(index + crate)
-        res_body = res.read().decode("utf-8")
-        v = getLatest(res_body)
-        if v == "":
-            cached = -1
-        else:
-            semver = v[0].split(".")
-            cached = 100 * int(semver[0]) + 10 * int(semver[1]) + int(semver[2])
-    else:
-        cached = int(cached)
-
     if mode == "stable":
+        if cached == "":
+            res = urllib.request.urlopen(index + crate)
+            res_body = res.read().decode("utf-8")
+            v = getLatest(res_body)
+            if v == "":
+                cached = -1
+            else:
+                semver = v[0].split(".")
+                cached = 100 * int(semver[0]) + 10 * int(semver[1]) + int(semver[2])
+        else:
+            cached = int(cached)
+
         # Get from crates.io
         res = urllib.request.urlopen(crates_io_api.replace("{CRATE}", crate))
         api_json = json.loads(res.read().decode("utf-8"))
@@ -80,8 +87,22 @@ def main(mode, index, crate):
         print("::set-output name=dl::" + latest[1][2])
         print("::set-output name=hash::" + latest[1][3])
     else:
-        print("nightly is not supported yet")
-        sys.exit(1)
+        if cached == "":
+            res = urllib.request.urlopen(index + crate)
+            res_body = res.read().decode("utf-8")
+            cached = getLatest(res_body)
+
+        # Get from GitHub
+        res = urllib.request.urlopen(github_api.replace("{REPO}", repo))
+        api_json = json.loads(res.read().decode("utf-8"))
+        sha = api_json["sha"]
+
+        if cached != sha:
+            with open(crate + ".cache", "w") as file:
+                file.write(sha)
+            print("::set-output name=build::true")
+        else:
+            print("::set-output name=build::false")
 
 
 if __name__ == "__main__":
