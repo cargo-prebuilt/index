5e7344258800c72f57f267e2109d569eab08f221
harmless-tech <28830529+harmless-tech@users.noreply.github.com>
2022-10-11 14:14:12 -0400

    Initial

---
+++ README.template.md
@@ -0,0 +1,12 @@
+# binary-cargo-tools
+Some crate.io binaries built for different platforms.
+
+## Binaries
+
+%%BINARIES%%
+
+## API
+
+- Index is a list of ids split by newlines. (https://github.com/crow-rest/binary-cargo-tools/releases/download/stable-index/index)
+- Each binary has it own index that stores the version info in a list split by newlines. [VERSION INDEX_LINK]
+- Following the INDEX_LINK gets you a newline split list of targets. [TARGET BINARY_ZIP BINARY_SHA256]
---
+++ gen.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+import sys
+
+
+def main(index):
+    if index == "stable":
+        with open("template.yml", "r") as file:
+            action_template = file.read()
+        with open("README.template.md", "r") as file:
+            readme_template = file.read()
+
+        with open("crate-list", "r") as file:
+            info = file.read().strip()
+
+        crates = info.split("\n")
+        readme = []
+        for c in crates:
+            data = c.strip().split(" ")
+            action = action_template.replace("%%CRATE%%", data[0])
+            action = action.replace("%%REPO%%", data[1])
+            action = action.replace("%%LICENSE%%", data[2])
+            action = action.replace("%%BINS%%", data[3])
+            action = action.replace("%%WIN_BINS%%", ",".join(map(lambda x: x + ".exe", data[3].split(","))))
+            with open("./.github/workflows/" + data[0] + ".yml", "w") as file:
+                file.write(action)
+
+            readme.append("- [" + data[0] + "](" + data[1] + ")")
+
+        readme = readme_template.replace("%%BINARIES%%", "\n".join(readme))
+        with open("README.md", "w") as file:
+            file.write(readme)
+    elif index == "nightly":
+        print("nightly not supported yet")
+        sys.exit(1)
+    else:
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    argv = sys.argv
+    main(argv[1])
