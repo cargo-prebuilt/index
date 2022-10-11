54f6e52c2fb10aaaa0a68e384bd2421da73bd86c
harmless-tech <28830529+harmless-tech@users.noreply.github.com>
2022-10-11 00:22:36 -0400

    Stable trunk builds (#2)
    * Create trunk.yml
    
    * Workflow stable checking
    
    * Collecter
    
    * Fixed dir issue
    
    * Env fix
    
    * Timestamp fix and release testing
    
    * Release fix
    
    * Some fixes
    
    * MacOS runner
    
    * Replaced release action
    
    * Release fix
    
    * Windows build
    
    * Powershell download fix
    
    * Cache fixes and removals
    
    * Added musl
    
    * Musl fix and MacOS aarch64
    
    * Fixes + DBG
    
    * Fixes
    
    * aarch64-musl
    
    * Fixed syntax
    
    * Hash fix and Dockerfile ARG fix
    
    * Small fixes
    
    * Attempt not found fix
    
    * DBG
    
    * DBG
    
    * More dbg
    
    * Pls work
    
    * ls
    
    * patch
    
    * ...
    
    * last test maybe
    
    * Test
    
    * Push to index at end
    
    * Increased timeout
    
    * Allow caching of docker images
    
    * Fixed caching
    
    * Hopefully better build performance for aarch64-linux
    
    * Missing \
    
    * more fixes
    
    * Improvements
    
    * Fixed wrong thing
    
    * Changed cache keys and permission fix for caching using docker
    
    * Cache fix?
    
    * Added targets to push index
    
    * Prerelease + newline fix
    
    * release stays prerelease fix?
    
    * check.py fix
    
    * fix?
    
    * fix?
    
    * Run everyday + Pull requests no longer push

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # binary-cargo-tools
-Some cargo tools built for different platforms
+Some cargo tools built for different platforms.
+
+## Tools
+- [trunk](https://github.com/thedodd/trunk)
---
+++ docker/entrypoint-alpine.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+export CFLAGS='-mno-outline-atomics'
+
+apk add --no-cache build-base python3
+
+cd /builder/build
+cargo build --release
+
+cd /builder
+./collect.py $BINS $LICENSE aarch64-unknown-linux-musl ./build ./build/target/release
