7f250a7b4f6a4226f47c42833aea816738aa84eb
harmless-tech <28830529+harmless-tech@users.noreply.github.com>
2022-10-11 13:52:35 -0400

    Stable cargo-edit (#5)
    * Initial workflow
    
    * Run collect.py outside of docker containers

--- docker/entrypoint-alpine.sh
+++ docker/entrypoint-alpine.sh
@@ -2,10 +2,7 @@
 
 export CFLAGS='-mno-outline-atomics'
 
-apk add --no-cache build-base python3 perl
+apk add --no-cache build-base perl openssl-dev
 
 cd /builder/build
 cargo build --release
-
-cd /builder
-./collect.py $BINS $LICENSE aarch64-unknown-linux-musl ./build ./build/target/release
--- docker/entrypoint-debian.sh
+++ docker/entrypoint-debian.sh
@@ -2,6 +2,3 @@
 
 cd /builder/build
 cargo build --release
-
-cd /builder
-./collect.py $BINS $LICENSE aarch64-unknown-linux-gnu ./build ./build/target/release
