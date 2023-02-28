e77bf934a57f7956a5c15838487388f9fb86f510
harmless-tech <28830529+harmless-tech@users.noreply.github.com>
2023-02-28 17:01:47 -0500

    Add target

--- scripts/gen.py
+++ scripts/gen.py
@@ -47,6 +47,7 @@ def main(mode, pull_request, crate, version, crate_license, dl, checksum, bins, 
 
         # Windows
         action = action.replace("%%WIN_64_BUILD%%", str("x86_64-pc-windows-msvc" not in unsupported))
+        action = action.replace("%%WIN_ARM64_BUILD%%", str("aarch64-pc-windows-msvc" not in unsupported))
         action = action.replace("%%WIN_32_BUILD%%", str("i686-pc-windows-msvc" not in unsupported))
 
         # Other optional
