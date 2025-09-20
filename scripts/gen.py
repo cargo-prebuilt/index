9673ca5409b0318f5d5569bc6b18c8506f4009d9
harmless-tech <git@harmless.tech>
2025-09-20 19:11:09 -0400

    Block riscv64gc-musl on crates that do not support it

--- scripts/gen.py
+++ scripts/gen.py
@@ -5,7 +5,7 @@ import misc
 
 t2_targets: list[str] = [
     "riscv64gc-unknown-linux-gnu",  # Optional Support (64-bit)
-    "riscv64gc-unknown-linux-musl", # TODO: ink-crater test!
+    "riscv64gc-unknown-linux-musl",
     "s390x-unknown-linux-gnu",
     "powerpc64le-unknown-linux-gnu",
     "armv7-unknown-linux-gnueabihf",  # Optional Support (32-bit)
